ae2762294691d11b88ba9d5727d4d415ca25d594
dummydota002 <154327520+dummydota002@users.noreply.github.com>
2023-12-20 06:32:29 +0700

    Update minecraft_setup.sh

--- minecraft_setup.sh
+++ minecraft_setup.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y default-jre screen
 
 # Download Minecraft server jar
 wget https://meta.fabricmc.net/v2/versions/loader/1.20.1/0.15.3/0.11.2/server/jar -O fabric-server-launcher.jar
+wget -P mods https://cdn.modrinth.com/data/MdwFAVRL/versions/YEzu8qAH/Cobblemon-fabric-1.4.0%2B1.20.1.jar
 
 # Run Fabric server installer
 java -jar fabric-server-launcher.jar server
