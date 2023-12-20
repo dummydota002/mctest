fdbec2b089f9667412f50547e380d7221b362280
dummydota002 <154327520+dummydota002@users.noreply.github.com>
2023-12-20 08:23:52 +0700

    Update minecraft_setup.sh

--- minecraft_setup.sh
+++ minecraft_setup.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install -y default-jre screen
 
 # Download Minecraft server jar
-wget https://meta.fabricmc.net/v2/versions/loader/1.20.1/0.15.3/0.11.2/server/jar -O server-launcher.jar
+wget https://meta.fabricmc.net/v2/versions/loader/1.20.1/0.15.3/0.11.2/server/jar -O fabric-server-launch.jar
 
 # Download Minecraft mod
 mkdir mods
@@ -13,7 +13,7 @@ wget -P mods https://cdn.modrinth.com/data/MdwFAVRL/versions/YEzu8qAH/Cobblemon-
 wget -P mods https://cdn.modrinth.com/data/P7dR8mSH/versions/YblXfKtI/fabric-api-0.91.0%2B1.20.1.jar
 
 # Run Fabric server installer
-java -jar server-launcher.jar server
+java -jar fabric-server-launch.jar server
 
 # Accept Minecraft EULA
 echo "eula=true" > eula.txt
