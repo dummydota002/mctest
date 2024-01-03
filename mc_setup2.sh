be1eed0d9927b1706b0ea4d85534fd1ea122cc41
dummydota002 <154327520+dummydota002@users.noreply.github.com>
2024-01-03 09:41:48 +0700

    Update mc_setup2.sh

--- mc_setup2.sh
+++ mc_setup2.sh
@@ -5,15 +5,7 @@ echo "eula=true" > eula.txt
 # allow crack
 echo "online-mode=false" > server.properties
 
-
-: <<'COMMENT'
-cat << EOF > start.sh
-#!/bin/bash
-java -Xmx2G -Xms1G -jar fabric-server-launch.jar nogui
-EOF
-COMMENT
-
 chmod +x start.sh
 
-wget https://github.com/playit-cloud/playit-agent/releases/download/v0.15.0/playit-linux-amd64
-chmod +x playit-linux-amd64
+wget https://github.com/playit-cloud/playit-agent/releases/download/v0.15.0/playit-linux-amd64 -o playit
+chmod +x playit
