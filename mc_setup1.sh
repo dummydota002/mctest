c18e0c34d29aa73e0bb9114408eae57c312f8b3a
dummydota002 <154327520+dummydota002@users.noreply.github.com>
2024-01-03 09:33:36 +0700

    Update mc_setup1.sh

--- mc_setup1.sh
+++ mc_setup1.sh
@@ -5,12 +5,12 @@ sudo apt-get update
 sudo apt-get install -y default-jre screen
 
 # Download Minecraft server jar
-wget https://meta.fabricmc.net/v2/versions/loader/1.20.1/0.15.3/0.11.2/server/jar -O fabric-server-launch.jar
+wget -O minecraft-launch.jar https://meta.fabricmc.net/v2/versions/loader/1.20.1/0.15.3/0.11.2/server/jar
 
 # Download Minecraft mod
 mkdir mods
 wget -P mods https://cdn.modrinth.com/data/P7dR8mSH/versions/YblXfKtI/fabric-api-0.91.0%2B1.20.1.jar
 
-# Run Fabric server installer
-java -jar fabric-server-launch.jar server
+# Run minecraft installer
+java -jar minecraft-launch.jar server
 stop
